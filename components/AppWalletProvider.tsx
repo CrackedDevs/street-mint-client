d6d757b7bbad2d10a704523d1c79eb5f4d393e62
Raghav Jindal <raghavjindal0212@gmail.com>
2024-10-10 18:00:14 +0530

    fixed wallet connection bug

--- components/AppWalletProvider.tsx
+++ components/AppWalletProvider.tsx
@@ -1,28 +1,22 @@
 "use client";
 
 import React, { useMemo } from "react";
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-export default function AppWalletProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const network =
-    process.env.NODE_ENV === "development"
-      ? WalletAdapterNetwork.Devnet
-      : WalletAdapterNetwork.Mainnet;
+import { SolflareWalletAdapter, UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+export default function AppWalletProvider({ children }: { children: React.ReactNode }) {
+  const network = process.env.NODE_ENV === "development" ? WalletAdapterNetwork.Devnet : WalletAdapterNetwork.Mainnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  const wallets = useMemo(() => [], []);
- 
+  const wallets = useMemo(() => {
+    return [new PhantomWalletAdapter(), new SolflareWalletAdapter(), new UnsafeBurnerWalletAdapter()];
+  }, []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
