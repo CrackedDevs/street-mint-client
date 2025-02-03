ba4ea1c744bfe0e79b3ab4ad5cd4a7e1b858a267
Vatsal Sanchala <bevatsal1122@gmail.com>
2025-02-03 21:56:21 +0530

    fix: minor fix

--- components/AdminNavBar.tsx
+++ components/AdminNavBar.tsx
@@ -8,7 +8,7 @@ export default function AdminNavBar() {
     <div>
       <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-white font-bold text-xl">Admin Panel</div>
+          <div className="text-white font-bold text-xl">StreetMint Admin Panel</div>
           <div className="space-x-4">
           <Link
               href="/admin"
@@ -17,7 +17,7 @@ export default function AdminNavBar() {
               Library
             </Link>
             <Link
-              href="/chip-settings"
+              href="/admin/chip-settings"
               className="text-white hover:text-gray-300"
             >
               Chips Manager
