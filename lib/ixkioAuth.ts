0e989899a120679b67d9168986b9eaa38ad15a21
Vatsal Sanchala <bevatsal1122@gmail.com>
2025-02-19 15:24:12 +0530

    fix: minor bug fix

--- lib/ixkioAuth.ts
+++ lib/ixkioAuth.ts
@@ -8,16 +8,18 @@ const AUTH_API_URL = "https://api.ixkio.com/v1/t";
 // After this, we won't be able to get pass again
 // Note: Do not use this function for anything else than checking auth status
 export const checkAuthStatus = async (x: string, n: string, e: string) => {
+  let collectibleData;
+  let resultData;
+  let response;
+  let isIRLtapped = false;
+
   try {
-
-    let isIRLtapped = false;
     const AUTH_INSTANCE_URL = `${AUTH_API_URL}?x=${x}&n=${n}&e=${e}`;
-    let resultData;
 
-    const collectibleData = await getCollectibleData(x, n);
+    collectibleData = await getCollectibleData(x, n);
     if (!collectibleData) return null;
 
-    const response = await axios.get(AUTH_INSTANCE_URL, {
+    response = await axios.get(AUTH_INSTANCE_URL, {
         headers: {
             "Content-Type": "application/json",
         },
@@ -36,7 +38,7 @@ export const checkAuthStatus = async (x: string, n: string, e: string) => {
     console.log("ixkio auth data", data);
     
     if (data && data.xuid === x && data.response.toLowerCase() === "pass") {
-        isIRLtapped = true;
+        isIRLtapped = true; // Only set to true here
         resultData = {
             status: "pass",
             collectibleData,
@@ -57,6 +59,16 @@ export const checkAuthStatus = async (x: string, n: string, e: string) => {
     return resultData
   } catch (error) {
     console.error("Error checking auth status", error);
+    if (collectibleData) {
+      resultData = {
+        status: "fail",
+        collectibleData,
+        scanCount: 0,
+        authenticated: false,
+        isIRLtapped
+      };
+      return resultData;
+    }
     return null;
   }
 }
