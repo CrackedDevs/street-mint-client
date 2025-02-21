c4651c81ea2972a5a5d7ec95c119fcb18d98989b
Vatsal Sanchala <bevatsal1122@gmail.com>
2025-02-21 16:01:17 +0530

    fix: iphone zooming effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta 
-          name="viewport" 
+        <meta
+          name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
       </head>
