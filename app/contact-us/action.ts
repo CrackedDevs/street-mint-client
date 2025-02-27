2a3f83890edc0af6f367d5888975859b1598f89a
Vatsal Sanchala <bevatsal1122@gmail.com>
2025-02-27 19:38:23 +0530

    feat: adds nodemailer to contact us form

--- app/contact-us/action.ts
+++ app/contact-us/action.ts
@@ -1,22 +1,52 @@
 "use server";
 
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-export async function sendEmail(name: string, email: string, body: string) {
-  const resend = new Resend(process.env.RESEND_API_KEY);
+export async function sendEmail(
+  brandName: string,
+  name: string,
+  email: string,
+  body: string
+) {
+  const platform = brandName == "IRLS" ? "IRLS" : "STREETMINT";
+
+  let fromEmail = "";
+  let fromName = "";
+  let app_password = "";
+  
+  if (platform == "STREETMINT") {
+    fromEmail = "hello@streetmint.xyz";
+    fromName = "Street Mint";
+    app_password = process.env.STREETMINT_NODEMAILER_APP_PASSWORD!;
+  } else {
+    fromEmail = "hello@irls.xyz";
+    fromName = "IRLS";
+    app_password = process.env.IRLS_NODEMAILER_APP_PASSWORD!;
+  }
 
   try {
-    const { data, error } = await resend.emails.send({
-      from: "Daryl <daryl@mail.irls.xyz>",
-      to: "Daryl <daryl@mail.irls.xyz>",
-      subject: "Query from Contact Us Form",
-      text: `Name: ${name}\nEmail: ${email}\n\nBody: ${body}`,
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: fromEmail,
+        pass: app_password,
+      },
     });
 
-    if (error) {
-      console.log(error);
-      return { success: false, message: "Failed to send email" };
-    }
+    const mailOptions = {
+      from: `${fromName} <${fromEmail}>`,
+      to: "bevatsal1122@gmail.com",
+      subject: `Query from Contact Us Form - ${brandName}`,
+      text: `Name: ${name}\nEmail: ${email}\n\nBody: ${body}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error: any, info: any) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
     return { success: true, message: "Email sent successfully" };
   } catch (emailError) {
     console.error("Error sending email:", emailError);
