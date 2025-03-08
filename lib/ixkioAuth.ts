50adbf99fa3e4b19cb63dbacf3e20d3f9775cdfa
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2025-03-08 09:22:24 +0530

    auth added

--- lib/ixkioAuth.ts
+++ lib/ixkioAuth.ts
@@ -44,19 +44,11 @@ export const checkAuthStatus = async (x: string, n: string, e: string, isCurrent
         throw new Error("Failed to authenticate with ixkio");
     }
 
-    // const data : {
-    //   UID: string;
-    //   xuid: string;
-    //   response: string;
-    // } = response.data;
-
-    const data = {
-      UID: "123",
-      xuid: x,
-      response: "pass"
-    }
-
-  
+    const data : {
+      UID: string;
+      xuid: string;
+      response: string;
+    } = response.data;
 
     console.log("ixkio auth data", data);
     
