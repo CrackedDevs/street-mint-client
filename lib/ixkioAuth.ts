39074312c9f618319894226fe038768e34624602
Vatsal Sanchala <bevatsal1122@gmail.com>
2025-05-01 13:51:23 +0530

    fix: removes testing code

--- lib/ixkioAuth.ts
+++ lib/ixkioAuth.ts
@@ -45,16 +45,11 @@ export const checkAuthStatus = async (x: string, n: string, e: string, isCurrent
         throw new Error("Failed to authenticate with ixkio");
     }
 
-    // const data : {
-    //   UID: string;
-    //   xuid: string;
-    //   response: string;
-    // } = response.data;
-
-    const data ={
-      xuid: x,
-      response: "Pass"
-    }
+    const data : {
+      UID: string;
+      xuid: string;
+      response: string;
+    } = response.data;
     
     console.log("ixkio auth data", data);
     
